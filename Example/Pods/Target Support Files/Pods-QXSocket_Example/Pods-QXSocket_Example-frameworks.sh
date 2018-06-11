fd3396cc23d9665440bc0fb4816a8ba1c0a96505
qingxu.chong <qingxu.chong@yintech.cn>
2018-06-11 14:01:28 +0800

    add qxsocket

--- Example/Pods/Target Support Files/Pods-QXSocket_Example/Pods-QXSocket_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-QXSocket_Example/Pods-QXSocket_Example-frameworks.sh
@@ -143,10 +143,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/QXSocket/QXSocket.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/BlueSocket/Socket.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/QXSocket/QXSocket.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/BlueSocket/Socket.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
---
+++ QXSocketManager.swift
@@ -0,0 +1,8 @@
+//
+//  QXSocketManager.swift
+//  QXSocket
+//
+//  Created by fox on 2018/6/11.
+//
+
+import Foundation
