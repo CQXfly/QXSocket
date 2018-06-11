07640c383e35086d7dfd4d8b7c07bba4ec8377e8
qingxu.chong <qingxu.chong@yintech.cn>
2018-06-11 09:51:46 +0800

    create

---
+++ Example/Pods/Target Support Files/Pods-QXSocket_Example/Pods-QXSocket_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QXSocket_Example : NSObject
+@end
+@implementation PodsDummy_Pods_QXSocket_Example
+@end
---
+++ Example/Pods/Target Support Files/QXSocket/QXSocket-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QXSocket : NSObject
+@end
+@implementation PodsDummy_QXSocket
+@end
