5882359f0ab89b3e697c73b2cffadd86db76bb83
qingxu.chong <qingxu.chong@yintech.cn>
2018-06-25 18:25:22 +0800

    socket 完成 读写

--- Example/QXSocket/ViewController.swift
+++ Example/QXSocket/ViewController.swift
@@ -7,18 +7,44 @@
 //
 
 import UIKit
+import QXSocket
 
 class ViewController: UIViewController {
 
+    var m : QXTCPSocketManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.m = QXTCPSocketManager.manager(host: "127.0.0.1", on: 6000)
+        
+        m?.connect()
+        
+        m?.delegate = self
     }
 
+    @IBOutlet weak var textInput: UITextField!
+    
+    @IBAction func sendButton(_ sender: Any) {
+        
+        m?.send(data: (textInput.text?.data(using: String.Encoding.utf8))!)
+        
+    }
+    
+    @IBOutlet weak var receiver: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension ViewController : QXSocketManagerProtocol {
+    func qx_socketDidReceiveData(_ data: Data) {
+        DispatchQueue.main.async {
+            self.receiver.text = String(data: data, encoding: .utf8)
+        }
+        
+    }
 }
 
